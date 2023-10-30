cb04329d1b78f4050ad557899ef9b676de809c2e
Gracy Arora <gracyarora812@gmail.com>
2023-10-30 13:36:48 +0530

    .

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div>
-        <div>
-            <h1>Login</h1>
+        <div className="bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacit-30 relative ">
+            <h1 className="text-4xl text-white font-bold text-center mb-6">Login</h1>
              <form action="">
-             <div>
+             <div className="relative my-4" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:text-white focus-border-blue-600 peer " >
                 <input type="email"/>
                 <label htmlFor="">Your Email</label>
              </div>
-             <div>
-                <input type="email"/>
-                <label htmlFor="">Your Email</label>
+             <div className="relative my-4" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:ring-0 focus:text-white focus-border-blue-600 peer " >
+                <input type="password"/>
+                <label htmlFor="">Your Password </label>
              </div>
             <div>
              <div>
@@ -21,13 +21,13 @@ const Login = () => {
                 <label htmlFor="Remember Me"></label>
              </div>
             <span>Forgot password?</span>
-            </div>
+            </div><ocus:outline-none></ocus:outline-none>
             <button type="submit">Login</button>
             <div>
                 <span>New here?<Link to='Register'>Create an Account </Link></span>
             </div>
              </form>
-        </div>
+        </div> 
       
     </div>
   );
